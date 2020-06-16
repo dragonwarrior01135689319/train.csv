c7d6b5abaaa2672270390eab9d8aa3b76e859ad7
dragonwarrior01135689319 <61971537+dragonwarrior01135689319@users.noreply.github.com>
2020-06-16 15:13:28 +0800

    Update and rename modelshort.py to modelshort(accuracy: 0.76555).py

--- modelshort(accuracy: 0.76555).py
+++ modelshort(accuracy: 0.76555).py
@@ -4,7 +4,7 @@ import math
 
 # processed train data
 family_content = []
-df = pd.read_csv(r'C:\Users\User-PC\Downloads\titanic_machine_learning\test.csv')
+df = pd.read_csv(r'C:\Users\User-PC\Downloads\titanic_machine_learning\train.csv')
 df = df.drop(['Name', 'Ticket', 'Cabin'], axis='columns')
 
 for i in range(len(df.PassengerId)):
@@ -30,7 +30,7 @@ model.fit(df[['Pclass', 'Age', 'Fare', 'Family', 'Q', 'S', 'male']], df.Survived
 
 # processed test data
 family_content = []
-df = pd.read_csv(r'C:\Users\User-PC\Downloads\titanic_machine_learning\train.csv')
+df = pd.read_csv(r'C:\Users\User-PC\Downloads\titanic_machine_learning\test.csv')
 df = df.drop(['Name', 'Ticket', 'Cabin'], axis='columns')
 
 for i in range(len(df.PassengerId)):
@@ -47,6 +47,18 @@ df = df.drop(['C', 'female'], axis='columns')
 
 df['Age'] = df['Age'].fillna(math.floor(df.Age.median()))
 # processed test data
+
+#finding nan spaces in the datasheet(throws error)
+#x=0
+#for i in range(len(df.PassengerId)):
+#    x=x+1
+#    math.floor(model.predict([[df.Pclass[i], df.Age[i], df.Fare[i], df.Family[i], df.Q[i], df.S[i], df.male[i]]]))
+#    print(x)
+#result: when i=152 there's a nan
+#print(df.Pclass[152], df.Age[152], df.Fare[152], df.Family[152], df.Q[152], df.S[152], df.male[152])
+#df.Fare[152]=nan
+
+df['Fare'] = df['Fare'].fillna(math.floor(df.Fare.median()))
 
 # predicting results
 resultId = []
